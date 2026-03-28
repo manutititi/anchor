40e941da6768f9cab37560a0a3953c2e559eeec7
manutititi <manuelgutierrezh12@gmail.com>
2026-03-28 14:29:39 +0100

    [FIX]-> sidecar broken

--- server/code/ui/router.py
+++ server/code/ui/router.py
@@ -14,39 +14,39 @@ def set_templates(t: Jinja2Templates):
 @router.get("", response_class=HTMLResponse)
 @router.get("/login", response_class=HTMLResponse)
 def ui_login(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+    return templates.TemplateResponse(request, "login.html")
 
 
 @router.get("/setup", response_class=HTMLResponse)
 def ui_setup(request: Request):
-    return templates.TemplateResponse("setup.html", {"request": request})
+    return templates.TemplateResponse(request, "setup.html")
 
 
 @router.get("/anchors", response_class=HTMLResponse)
 def ui_anchors(request: Request):
-    return templates.TemplateResponse("anchors.html", {"request": request})
+    return templates.TemplateResponse(request, "anchors.html")
 
 
 @router.get("/secrets", response_class=HTMLResponse)
 def ui_secrets(request: Request):
-    return templates.TemplateResponse("secrets.html", {"request": request})
+    return templates.TemplateResponse(request, "secrets.html")
 
 
 @router.get("/users", response_class=HTMLResponse)
 def ui_users(request: Request):
-    return templates.TemplateResponse("users.html", {"request": request})
+    return templates.TemplateResponse(request, "users.html")
 
 
 @router.get("/logs", response_class=HTMLResponse)
 def ui_logs(request: Request):
-    return templates.TemplateResponse("logs.html", {"request": request})
+    return templates.TemplateResponse(request, "logs.html")
 
 
 @router.get("/integrations", response_class=HTMLResponse)
 def ui_integrations(request: Request):
-    return templates.TemplateResponse("integrations.html", {"request": request})
+    return templates.TemplateResponse(request, "integrations.html")
 
 
 @router.get("/wireguard", response_class=HTMLResponse)
 def ui_wireguard(request: Request):
-    return templates.TemplateResponse("wireguard.html", {"request": request})
+    return templates.TemplateResponse(request, "wireguard.html")
